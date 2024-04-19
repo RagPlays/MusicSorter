f3202c6245321841139a4db003f7bd56a6943d7d
Maurice Vandenheede <maurice.vandenheede@gmail.com>
2024-04-19 22:42:36 +0200

    Completed

--- MusicSorter/main.cpp
+++ MusicSorter/main.cpp
@@ -2,72 +2,233 @@
 #include <filesystem>
 #include <algorithm>
 #include <string>
+#include <cassert>
+#include <windows.h>
 
 namespace fs = std::filesystem;
 
-void SortMusicFiles(const fs::path& path)
+void EnableVirtualTerminalProcessing()
 {
+    // Get the handle to the standard output device.
+    HANDLE hOut{ GetStdHandle(STD_OUTPUT_HANDLE) };
+
+    // Retrieve the current console mode.
+    DWORD dwMode{};
+    GetConsoleMode(hOut, &dwMode);
+
+    // Enable the virtual terminal processing mode.
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+}
+
+void ClearConsole();
+void PrintGreenText(const std::string& text);
+void PrintRedText(const std::string& text);
+void PrintLightBlueText(const std::string& text);
+void PrintOrderCompleted(const std::string& filename);
+void PrintOrderFailed(const std::string& filename);
+void PrintOrderFailedException(const std::string& execption);
+
+const std::string GetDirectory();
+bool GetIsTestRun();
+
+void SortMusicFiles(const fs::path& path, bool isTestRun)
+{
+    int filesSorted{};
+    int filesNotSorted{};
+
     for (const auto& entry : fs::directory_iterator(path))
     {
+        // dont go into folders
+        if (entry.is_directory()) continue;
+
+        if (!fs::is_regular_file(entry.status()))
+        {
+            PrintRedText("...No regular entry...\n");
+            PrintRedText("There is a file that is not a regular file.\n");
+            continue;
+        }
+
         try
         {
-            std::string filename = entry.path().filename().string();
-            std::size_t dashPos = filename.find('-');
+            const std::string filename{ entry.path().filename().string() };
+            std::cout << "Ordering: " << filename << "\n";
+            const std::size_t dashPos{ filename.find('-') };
 
-            if (dashPos != std::string::npos)
+            if (dashPos == std::string::npos || fs::exists(filename))
             {
-                std::string artistName = filename.substr(0, dashPos);
+                PrintOrderFailed(filename);
+                ++filesNotSorted;
+                continue;
+            }
 
-                // Trim spaces at both ends
-                artistName.erase(
-                    artistName.begin(),
-                    std::find_if(artistName.begin(), artistName.end(),
-                        [](unsigned int ch)
-                        {
-                            return !std::isspace(ch);
-                        }
-                    )
-                );
+            std::string artistName{ filename.substr(0, dashPos) };
 
-                artistName.erase(
-                    std::find_if(
-                        artistName.rbegin(), artistName.rend(),
-                        [](unsigned int ch)
-                        {
-                            return !std::isspace(ch);
-                        }
-                ).base(), artistName.end());
+            // Trim spaces at both ends and replace remaining spaces with underscores
+            auto left{ std::find_if_not(artistName.begin(), artistName.end(), ::isspace) };
+            auto right{ std::find_if_not(artistName.rbegin(), artistName.rend(), ::isspace).base() };
+            artistName = std::string{ left, right };
+            std::replace(artistName.begin(), artistName.end(), ' ', '_');
 
-                // Replace remaining spaces with underscores
-                std::replace(artistName.begin(), artistName.end(), ' ', '_');
+            const fs::path artistDir{ path / artistName };
 
-                fs::path artistDir = path / artistName;
+            if (!isTestRun)
+            {
                 if (!fs::exists(artistDir))
                 {
                     fs::create_directory(artistDir);
                 }
-
                 fs::rename(entry.path(), artistDir / filename);
             }
-            else
-            {
-                std::cout << "File not following 'Artist Name - Song Name' format: " << filename << "\n";
-            }
+
+            PrintOrderCompleted(filename);
+            ++filesSorted;
         }
-        catch (const std::system_error& exception)
+        catch (const std::exception& exception)
         {
-            std::cout << "Caught exception: " << exception.what() << "\t\t File will be skipped!\n";
+            PrintOrderFailedException(std::string{ exception.what() });
+            ++filesNotSorted;
         }
+    }
+
+    PrintLightBlueText("\n\n --------------- Done ---------------\n");
+
+    PrintLightBlueText("Total ordering attemps: ");
+    std::cout << std::to_string(filesSorted + filesNotSorted) << "\n";
+
+    PrintLightBlueText("Orderings completed: ");
+    PrintGreenText(std::to_string(filesSorted) + "\n");
+
+    PrintLightBlueText("Orderings failed: ");
+    PrintRedText(std::to_string(filesNotSorted) + "\n");
+}
+
+void ClearConsole()
+{
+    std::cout << "\033c";
+}
+
+void PrintGreenText(const std::string& text)
+{
+    // Set Color Green
+    std::cout << "\033[1;32m";
+    // Print Text
+    std::cout << text;
+    // Reset Color
+    std::cout << "\033[0m";
+}
+
+void PrintRedText(const std::string& text)
+{
+    // Set Color Red
+    std::cout << "\033[1;31m";
+    // Print Text
+    std::cout << text;
+    // Reset Color
+    std::cout << "\033[0m";
+}
+
+void PrintLightBlueText(const std::string& text)
+{
+    // Set Color LightBlue
+    std::cout << "\033[1;34m";
+    // Print Text
+    std::cout << text;
+    // Reset Color
+    std::cout << "\033[0m";
+}
+
+void PrintOrderCompleted(const std::string& filename)
+{
+    PrintGreenText("Ordering completed with file: " + filename + "\n");
+}
+
+void PrintOrderFailed(const std::string& filename)
+{
+    PrintRedText("Ordering failed with file: " + filename + "\n");
+}
+
+void PrintOrderFailedException(const std::string& execptionWhat)
+{
+    PrintRedText("Caught exception: " + execptionWhat + "\t File will be skipped!\n");
+}
+
+const std::string GetDirectory()
+{
+    std::string musicDir;
+    PrintLightBlueText("Enter the path to your music directory: \n");
+    do
+    {
+        std::getline(std::cin, musicDir);
+        if (fs::exists(musicDir) && musicDir.size() > 1)
+        {
+            break;
+        }
+        else
+        {
+            PrintRedText("Path incorrect, fill in existing pathname.\n");
+        }
+    } while (true);
+
+    std::cout << "\n";
+    return musicDir;
+}
+
+bool GetIsTestRun()
+{
+    PrintLightBlueText("Do you want to do a file test run? ");
+    std::cout << "Y/N\n";
+    std::string input;
+    do
+    {
+        std::getline(std::cin, input);
+
+        if (input.size() < 1) continue;
+
+        if (input != "Y" && input != "y" && input != "N" && input != "n")
+        {
+            PrintRedText("Invalid input. Please try again.\n");
+        }
+    }
+    while (input != "Y" && input != "y" && input != "N" && input != "n");
+
+    std::cout << "\n";
+
+    switch (input[0])
+    {
+    case 'Y':
+    case 'y':
+        return true;
+        break;
+    case 'N':
+    case 'n':
+        return false;
+        break;
+    default:
+        assert(false);
+        throw std::exception{};
+        break;
     }
 }
 
 int main()
 {
-    std::string musicDir;
-    std::cout << "Enter the path to your music directory: \n";
-    std::getline(std::cin, musicDir);
+    EnableVirtualTerminalProcessing();
 
-    SortMusicFiles(musicDir);
+    const std::string dir{ GetDirectory() };
+    bool isTestRun{ GetIsTestRun() };
 
+    if (isTestRun)
+    {
+        PrintLightBlueText(" ---- TestRun is starting ----\n");
+    }
+    else
+    {
+        PrintLightBlueText(" ---- Sorting is starting ----\n");
+    }
+
+    SortMusicFiles(dir, isTestRun);
+
+    std::cin.get();
     return 0;
 }
