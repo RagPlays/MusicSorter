e60979c3ee972c16897261e922d193277af4041a
Maurice Vandenheede <maurice.vandenheede@gmail.com>
2024-04-21 12:02:01 +0200

    Added features
    - Added Error Logging to have all errors in display after each other.
    Every Error has a ErrorType and this will be shown as well.
    
    - Made better visuals in console.
    
    - Added more checks to the files.

--- MusicSorter/main.cpp
+++ MusicSorter/main.cpp
@@ -4,24 +4,62 @@
 #include <string>
 #include <cassert>
 #include <windows.h>
+#include <regex>
+
+enum class ErrorType
+{
+    NON_REGULAR_FILE,
+    EXEPTIONTHROW,
+    WRONG_EXTENTION,
+    MULTIPLE_SPACES,
+    WRONG_FORMAT,
+    UNSUPPORTED_CHARACTERS
+};
+
+struct Error
+{
+    explicit Error(ErrorType errorType, const std::string& filename)
+        : errorType{ errorType }
+        , filename{ filename }
+    {
+    }
+    explicit Error(ErrorType errorType)
+        : errorType{ errorType }
+        , filename{ std::string{} }
+    {
+        switch (errorType)
+        {
+        case ErrorType::NON_REGULAR_FILE:
+        case ErrorType::EXEPTIONTHROW:
+        default:
+            // Can not be right need to use Error(ErrorType errorType, const std::string& filename) instead
+            assert(false);
+            break;
+        }
+    }
+    ErrorType errorType;
+    std::string filename;
+};
+
+struct FolderData
+{
+    size_t readableFileCount{};
+    size_t folderCount{};
+    size_t unreadableFileCount{};
+    size_t hiddenFileCount{};
+};
+
+std::vector<Error> g_Errors{};
+const std::regex g_MultiSpace{ "  +" };
 
 namespace fs = std::filesystem;
 
-void EnableVirtualTerminalProcessing()
-{
-    // Get the handle to the standard output device.
-    HANDLE hOut{ GetStdHandle(STD_OUTPUT_HANDLE) };
-
-    // Retrieve the current console mode.
-    DWORD dwMode{};
-    GetConsoleMode(hOut, &dwMode);
-
-    // Enable the virtual terminal processing mode.
-    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-    SetConsoleMode(hOut, dwMode);
-}
-
+void SortMusicFiles(const fs::path& path, bool isTestRun);
+void PrintFolderInfo(const FolderData& folderData);
+void PrintErrorLog();
+void EnableVirtualTerminalProcessing();
 void ClearConsole();
+void PrintRegularText(const std::string& text);
 void PrintGreenText(const std::string& text);
 void PrintRedText(const std::string& text);
 void PrintLightBlueText(const std::string& text);
@@ -29,44 +67,54 @@ void PrintOrderCompleted(const std::string& filename);
 void PrintOrderFailed(const std::string& filename);
 void PrintWrongExtention(const std::string& filename);
 void PrintOrderFailedException(const std::string& execption);
-
 const std::string GetDirectory();
-bool GetIsTestRun();
+bool GetYesNo(const std::string& question);
+bool IsError(fs::directory_entry entry, const std::string& filename, size_t dashPos);
+bool IsHiddenFile(const fs::path& p);
+FolderData GetFileData(const fs::path& path);
 
 void SortMusicFiles(const fs::path& path, bool isTestRun)
 {
-    int filesSorted{};
-    int filesNotSorted{};
+    if (isTestRun)
+    {
+        PrintLightBlueText(" ---- TestRun is starting ----\n");
+    }
+    else
+    {
+        PrintLightBlueText(" ---- Sorting is starting ----\n");
+    }
+
+    size_t nrFilesSorted{};
+    size_t nrFilesNotSorted{};
 
     for (const auto& entry : fs::directory_iterator(path))
     {
         // dont go into folders
         if (entry.is_directory()) continue;
 
+        // skip hidden files
+        if (IsHiddenFile(entry.path())) continue;
+
         if (!fs::is_regular_file(entry.status()))
         {
             PrintRedText("...No regular entry...\n");
             PrintRedText("There is a file that is not a regular file.\n");
+            g_Errors.emplace_back(Error{ ErrorType::NON_REGULAR_FILE });
+            ++nrFilesNotSorted;
             continue;
         }
 
         try
         {
             const std::string filename{ entry.path().filename().string() };
-            std::cout << "Ordering: " << filename << "\n";
+            std::cout << "Sorting: " << filename << "\n";
             const std::size_t dashPos{ filename.find('-') };
 
-            if (entry.path().extension() != ".mp3")
-            {
-                PrintWrongExtention(filename);
-                ++filesNotSorted;
-                continue;
-            }
-
-            if (dashPos == std::string::npos || fs::exists(filename))
+            // Checks
+            if (IsError(entry, filename, dashPos))
             {
                 PrintOrderFailed(filename);
-                ++filesNotSorted;
+                ++nrFilesNotSorted;
                 continue;
             }
 
@@ -90,30 +138,118 @@ void SortMusicFiles(const fs::path& path, bool isTestRun)
             }
 
             PrintOrderCompleted(filename);
-            ++filesSorted;
+            ++nrFilesSorted;
         }
         catch (const std::exception& exception)
         {
             PrintOrderFailedException(std::string{ exception.what() });
-            ++filesNotSorted;
+            g_Errors.emplace_back(Error{ ErrorType::EXEPTIONTHROW });
+            ++nrFilesNotSorted;
         }
     }
 
     PrintLightBlueText("\n\n --------------- Done ---------------\n");
 
-    PrintLightBlueText("Total ordering attemps: ");
-    std::cout << std::to_string(filesSorted + filesNotSorted) << "\n";
+    PrintLightBlueText(" Total sorting attemps: ");
+    std::cout << std::to_string(nrFilesSorted + nrFilesNotSorted) << "\n";
 
-    PrintLightBlueText("Orderings completed: ");
-    PrintGreenText(std::to_string(filesSorted) + "\n");
+    PrintLightBlueText(" Sorting completed: ");
+    PrintGreenText(std::to_string(nrFilesSorted) + "\n");
 
-    PrintLightBlueText("Orderings failed: ");
-    PrintRedText(std::to_string(filesNotSorted) + "\n");
+    PrintLightBlueText(" Sorting failed: ");
+    PrintRedText(std::to_string(nrFilesNotSorted) + "\n\n");
+}
+
+void PrintFolderInfo(const FolderData& folderData)
+{
+    PrintLightBlueText(" ------------ FolderInfo ------------\n");
+    PrintLightBlueText(" Folders found: ");
+    PrintRegularText(std::to_string(folderData.folderCount) + "\n");
+    PrintLightBlueText(" Readable files found: ");
+    PrintRegularText(std::to_string(folderData.readableFileCount) + "\n");
+    PrintLightBlueText(" Unreadable filed found: ");
+    PrintRegularText(std::to_string(folderData.unreadableFileCount) + "\n");
+    PrintLightBlueText(" Hidden files found: ");
+    PrintRegularText(std::to_string(folderData.hiddenFileCount) + " (these files are ignored)\n\n");
+}
+
+void PrintErrorLog()
+{
+    if (g_Errors.empty()) return;
+
+    if (!GetYesNo("Do you want a error log?")) return;
+
+    PrintLightBlueText("\n ------------- ErrorLog -------------\n");
+
+    size_t nrNonRegularFiles{};
+    size_t nrExecptionsThrown{};
+
+    for (const auto& error : g_Errors)
+    {
+        switch (error.errorType)
+        {
+        case ErrorType::NON_REGULAR_FILE:
+            ++nrNonRegularFiles;
+            break;
+        case ErrorType::EXEPTIONTHROW:
+            ++nrExecptionsThrown;
+            break;
+        case ErrorType::WRONG_EXTENTION:
+            PrintRedText("WRONG FILE EXTENTION::" + error.filename + "\n");
+            break;
+        case ErrorType::MULTIPLE_SPACES:
+            PrintRedText("MULTIPLE SPACES::" + error.filename + "\n");
+            break;
+        case ErrorType::WRONG_FORMAT:
+            PrintRedText("WRONG FORMAT EXTENTION::" + error.filename + "\n");
+            break;
+        case ErrorType::UNSUPPORTED_CHARACTERS:
+            PrintRedText("UNSUPPORTED CHARACTERS::" + error.filename + "\n");
+            break;
+        default:
+            PrintRegularText("There was a unknown error.\n");
+            break;
+        }
+    }
+
+    if (nrNonRegularFiles)
+    {
+        PrintRedText("There where ");
+        PrintRegularText(std::to_string(nrNonRegularFiles));
+        PrintRedText(" non-regular files found.\n");
+    }
+    if (nrExecptionsThrown)
+    {
+        PrintRedText("There where ");
+        PrintRegularText(std::to_string(nrExecptionsThrown));
+        PrintRedText(" exceptions thrown. (undefined error)\n");
+    }
+
+    g_Errors.clear();
+}
+
+void EnableVirtualTerminalProcessing()
+{
+    // Get the handle to the standard output device.
+    HANDLE hOut{ GetStdHandle(STD_OUTPUT_HANDLE) };
+
+    // Retrieve the current console mode.
+    DWORD dwMode{};
+    GetConsoleMode(hOut, &dwMode);
+
+    // Enable the virtual terminal processing mode.
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
 }
 
 void ClearConsole()
 {
     std::cout << "\033c";
+}
+
+void PrintRegularText(const std::string& text)
+{
+    std::cout << text;
 }
 
 void PrintGreenText(const std::string& text)
@@ -168,7 +304,7 @@ void PrintOrderFailedException(const std::string& execptionWhat)
 
 const std::string GetDirectory()
 {
-    std::string musicDir;
+    std::string musicDir{};
     PrintLightBlueText("Enter the path to your music directory: \n");
     do
     {
@@ -183,14 +319,14 @@ const std::string GetDirectory()
         }
     } while (true);
 
-    std::cout << "\n";
+    PrintRegularText("\n");
     return musicDir;
 }
 
-bool GetIsTestRun()
+bool GetYesNo(const std::string& question)
 {
-    PrintLightBlueText("Do you want to do a file test run? ");
-    std::cout << "Y/N\n";
+    PrintLightBlueText(question);
+    PrintRegularText(" Y/N\n");
     std::string input;
     do
     {
@@ -205,7 +341,7 @@ bool GetIsTestRun()
     }
     while (input != "Y" && input != "y" && input != "N" && input != "n");
 
-    std::cout << "\n";
+    PrintRegularText("\n");
 
     switch (input[0])
     {
@@ -224,23 +360,88 @@ bool GetIsTestRun()
     }
 }
 
+bool IsError(fs::directory_entry entry, const std::string& filename, size_t dashPos)
+{
+    for (char c : filename)
+    {
+        if (!std::isprint(static_cast<unsigned char>(c)))
+        {
+            g_Errors.emplace_back(Error{ ErrorType::UNSUPPORTED_CHARACTERS, filename });
+            return true;
+        }
+    }
+
+    bool isThereError{ false };
+    if (entry.path().extension() != ".mp3")
+    {
+        isThereError = true;
+        g_Errors.emplace_back(Error{ ErrorType::WRONG_EXTENTION, filename });
+    }
+    else if (dashPos == std::string::npos)
+    {
+        isThereError = true;
+        g_Errors.emplace_back(Error{ ErrorType::WRONG_FORMAT, filename });
+    }
+    else if (std::regex_search(filename, g_MultiSpace))
+    {
+        isThereError = true;
+        g_Errors.emplace_back(Error{ ErrorType::MULTIPLE_SPACES, filename });
+    }
+    return isThereError;
+}
+
+bool IsHiddenFile(const fs::path& p)
+{
+    DWORD attributes{ GetFileAttributesW(p.c_str()) };
+
+    return (attributes != INVALID_FILE_ATTRIBUTES &&
+        (attributes & FILE_ATTRIBUTE_HIDDEN || attributes & FILE_ATTRIBUTE_SYSTEM));
+}
+
+FolderData GetFileData(const fs::path& path)
+{
+    FolderData folderData{};
+    for (const auto& entry : fs::directory_iterator(path))
+    {
+        if (entry.is_directory())
+        {
+            ++folderData.folderCount;
+            continue;
+        }
+
+        if (!fs::is_regular_file(entry.status()))
+        {
+            ++folderData.unreadableFileCount;
+            continue;
+        }
+
+        if (IsHiddenFile(entry.path()))
+        {
+            ++folderData.hiddenFileCount;
+            continue;
+        }
+
+        ++folderData.readableFileCount;
+    }
+
+    return folderData;
+}
+
 int main()
 {
     EnableVirtualTerminalProcessing();
 
-    const std::string dir{ GetDirectory() };
-    bool isTestRun{ GetIsTestRun() };
+    const fs::path dir{ GetDirectory() };
+    const bool isTestRun{ GetYesNo("Do you want to do a file test run?")};
 
-    if (isTestRun)
-    {
-        PrintLightBlueText(" ---- TestRun is starting ----\n");
-    }
-    else
-    {
-        PrintLightBlueText(" ---- Sorting is starting ----\n");
-    }
+    FolderData folderData{ GetFileData(dir) };
+    g_Errors.reserve(folderData.readableFileCount / 3);
 
     SortMusicFiles(dir, isTestRun);
+
+    PrintFolderInfo(folderData);
+
+    PrintErrorLog();
 
     std::cin.get();
     return 0;
